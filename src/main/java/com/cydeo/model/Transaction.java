b59847016a1e49aea7135130e61e7976f92c90e7
symcetin <seyma.oskurcu@gmail.com>
2022-09-10 10:13:24 -0400

    bank simulation app

---
+++ src/main/java/com/cydeo/model/Transaction.java
@@ -0,0 +1,15 @@
+package com.cydeo.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.UUID;
+
+public class Transaction {
+
+    private UUID sender;
+    private UUID receiver;
+    private BigDecimal amount;
+    private String message;
+    private Date creationDate;
+
+}
