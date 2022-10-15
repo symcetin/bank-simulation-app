cd94b63030d8d30cd544043cbcd2742a5c41ea81
symcetin <seyma.oskurcu@gmail.com>
2022-10-15 11:39:50 -0400

    account validation is completed

--- src/main/java/com/cydeo/controller/AccountController.java
+++ src/main/java/com/cydeo/controller/AccountController.java
@@ -6,11 +6,13 @@ import com.cydeo.model.Account;
 import com.cydeo.service.AccountService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.Date;
 import java.util.UUID;
 
@@ -48,8 +50,12 @@ public class AccountController {
      //create a /create post method that creates account in the service
     //then return the index page
     @PostMapping("/create")
-    public String createAccount(@ModelAttribute("account") Account account){
+    public String createAccount(@Valid @ModelAttribute("account") Account account, BindingResult bindingResult, Model model){
 
+         if(bindingResult.hasErrors()){
+             model.addAttribute("accountTypes", AccountType.values());
+             return "account/create-account";
+         }
          accountService.createNewAccount(account.getBalance(),new Date(),
                  account.getAccountType(),account.getUserId());
 
