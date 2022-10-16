0fada9fab5a7b10b021a1179e73cf064c6b00d82
symcetin <seyma.oskurcu@gmail.com>
2022-10-15 20:42:18 -0400

    transaction validation is completed

--- src/main/java/com/cydeo/controller/TransactionController.java
+++ src/main/java/com/cydeo/controller/TransactionController.java
@@ -7,11 +7,13 @@ import com.cydeo.service.TransactionService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.Date;
 import java.util.UUID;
 
@@ -39,8 +41,13 @@ public class TransactionController {
     //write a post method, that takes transaction object from the method above,
     //complete the make transfer and return the same page.
     @PostMapping("/transfer")
-    public String postMakeTransfer(@ModelAttribute("transaction") Transaction transaction,Model model){
+    public String postMakeTransfer(@Valid @ModelAttribute("transaction") Transaction transaction, BindingResult bindingResult, Model model){
 
+        if(bindingResult.hasErrors()){
+
+            model.addAttribute("accounts",accountService.listAllAccount());
+            return "transaction/make-transfer";
+        }
         //I have UUID but I need to provide Account to make transfer method.
         Account sender = accountService.retrieveById(transaction.getSender());
         Account receiver = accountService.retrieveById(transaction.getReceiver());
