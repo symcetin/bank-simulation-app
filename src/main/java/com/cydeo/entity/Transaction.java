8d853b93d43d279ad1d74672831c68d5e2d870f6
symcetin <seyma.oskurcu@gmail.com>
2022-11-19 11:44:54 -0500

    entities are added

---
+++ src/main/java/com/cydeo/entity/Transaction.java
@@ -0,0 +1,36 @@
+package com.cydeo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "transactions")
+@Entity
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Account sender;
+
+    @ManyToOne
+    private Account receiver;
+
+    private BigDecimal amount;
+
+    private String message;
+
+    @Column(columnDefinition = "TIMESTAMP")
+    private Date creationDate;
+}
