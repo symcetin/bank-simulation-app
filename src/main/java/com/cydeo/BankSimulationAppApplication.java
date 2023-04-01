14b6e399586c9af29daf5b95735c3b33a6f73f11
symcetin <seyma.oskurcu@gmail.com>
2023-04-01 11:52:01 -0400

    project completed

--- src/main/java/com/cydeo/BankSimulationAppApplication.java
+++ src/main/java/com/cydeo/BankSimulationAppApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class BankSimulationAppApplication {
@@ -41,6 +43,10 @@ public class BankSimulationAppApplication {
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
